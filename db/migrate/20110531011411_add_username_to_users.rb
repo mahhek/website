951479f01c28a5b16e7d17918c70c0d06194ab62
hamid <hamid@localhost.(none)>
2011-06-12 17:36:53 +0500

    db issue

--- db/migrate/20110531011411_add_username_to_users.rb
+++ db/migrate/20110531011411_add_username_to_users.rb
@@ -1,17 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :username, :string
-    Role.create(:name => "Admin")
-    Role.create(:name => "Staff")
-    Role.create(:name => "Coach")
-    Role.create(:name => "Composer")
-    Role.create(:name => "Business")
-    Role.create(:name => "Venue")
-    Role.create(:name => "School")
-
-    user = User.new(:email => "admin@website.com", :password => "admin123", :first_name => "admin", :last_name => "admin" )
-    user.roles << Role.find_by_name("Admin")
-    user.save!
   end
 
   def self.down
