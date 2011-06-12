d5f17f6a7152ba31840850c2496f862aa74fba75
hamid <hamid@localhost.(none)>
2011-06-12 18:23:46 +0500

    deployment issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
+require 'lib/util'
+require 'lib/google'
 class ApplicationController < ActionController::Base
-
   protect_from_forgery
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Website::Application.initialize!
-require 'lib/util'
-require 'lib/util'
-require 'lib/google'
